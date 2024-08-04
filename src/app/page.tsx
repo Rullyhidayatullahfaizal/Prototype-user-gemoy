6404c778f28a07cff8bdd8e89285e89e249ca6f6
Rully_H-F <Rullyhidayatullahfaizal@gmail.com>
2024-08-05 00:20:39 +0700

    loginPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+"use client"
+
 import ButtonUniversal from "@/components/button";
 import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const router = useRouter()
+
+
+  const handleLogin = () => {
+    router.push('/login')
+  }
+
   return (
     <>
     <div className="relative">
@@ -13,9 +24,10 @@ export default function Home() {
       </p>
       <p className="mt-5 text-sm md:text-base lg:text-lg bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">Lorem ipsum dolor sit amet consectetur adipisicing elit . <br /> Iure atque nemo velit incidunt ab harum laborum delectus, repellat porro numquam aut dolorum <br />? Aperiam suscipit , minus eveniet omnis iusto facilis ut!</p>
       <div className="mt-10 md:mt-5 lg:mt-0 pointer-events-auto">
-        <ButtonUniversal >
-          Daftar sekarang
-          </ButtonUniversal>
+        <ButtonUniversal 
+              onClick={handleLogin} 
+              label="Login Now"
+            />
       </div>
     </div>
   </BackgroundGradientAnimation>
